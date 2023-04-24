64f3ddda5a25330bda41d164771895c311b0870c
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-04-24 17:59:46 -0500

    End of day 1

--- src/main/java/org/example/Contacts.java
+++ src/main/java/org/example/Contacts.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +12,7 @@ public class Contacts {
     public static void main(String[] args) {
         String directory = "data";
 
-        String filename = "contacts.txt";
+        String filename = "contacts.csv";
 
         Path dataDirectory = Paths.get(directory);
 
@@ -36,13 +37,43 @@ public class Contacts {
         AddContact contact3 = new AddContact("Tammy", 1234567891);
         AddContact contact4 = new AddContact("Tina", 1234567891);
         AddContact contact5 = new AddContact("Johny", 1234567891);
-        List<Object> contacts = new ArrayList<>();
+        AddContact contact6 = new AddContact("Billy", 1234567891);
+        List<String> contacts = new ArrayList<>();
+
         contacts.add(contact1.getName() + ": " + contact1.getPhoneNumber());
         contacts.add(contact2.getName() + ": " + contact2.getPhoneNumber() );
         contacts.add(contact3.getName() + ": " + contact3.getPhoneNumber() );
         contacts.add(contact4.getName() + ": " + contact4.getPhoneNumber() );
         contacts.add(contact5.getName() + ": " + contact5.getPhoneNumber() );
+        contacts.add(contact6.getName() + ": " + contact6.getPhoneNumber() );
         System.out.println(contacts);
 
-    }
+        for (String contact : contacts) {
+            contacts.add(contact);
+
+        }
+
+        try {
+           Files.write(dataFile, contacts);
+
+    } catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+        try{
+            List<String> contactListFile = Files.readAllLines(dataFile);
+            for(String line : contactListFile){
+                String[] contactInfo = line.split(":");
+                String name = contactInfo[0];
+                String number = contactInfo[1];
+                System.out.println(name + number);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+}
 }
