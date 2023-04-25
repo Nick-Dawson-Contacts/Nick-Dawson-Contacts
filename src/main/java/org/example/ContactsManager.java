d445c1ddc6494ce79c236caa8a70c42d34a4891b
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-04-25 11:07:57 -0500

    added remove method

--- src/main/java/org/example/ContactsManager.java
+++ src/main/java/org/example/ContactsManager.java
@@ -3,5 +3,6 @@ package org.example;
 public class ContactsManager {
     public static void main(String[] args) {
         ContactsFile contacts = new ContactsFile();
+        contacts.deleteContact();
     }
 }
