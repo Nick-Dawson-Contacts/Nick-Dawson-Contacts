ccba3c11a5344f52de587594c228bdd7f41ad561
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-04-25 14:50:35 -0500

    finished

--- src/main/java/org/example/ContactsManager.java
+++ src/main/java/org/example/ContactsManager.java
@@ -6,16 +6,11 @@ import java.util.Scanner;
 public class ContactsManager {
     public static void main(String[] args) throws IOException {
         ContactsFile contacts = new ContactsFile();
-//        contacts.printContacts();
-//        contacts.addContact();
-//        contacts.deleteContact();
-//        contacts.searchContact();
         Scanner scanner = new Scanner(System.in);
-//        Menu homeScreen = new Menu();
-//        System.out.println(homeScreen);
-        System.out.println("1-5");
-        int input = Integer.parseInt(scanner.nextLine());
-
+        while(true){
+            System.out.printf("---------------\n1. View Contacts.\n2. Add a new contact.\n3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):\n" );
+            int input = Integer.parseInt(scanner.nextLine());
             switch (input) {
                 case 1:
                     input = 1;
@@ -27,17 +22,19 @@ public class ContactsManager {
                     break;
                 case 3:
                     input = 3;
-                    contacts.deleteContact();
+                    contacts.searchContact();
                     break;
                 case 4:
                     input = 4;
-                    contacts.searchContact();
+                    contacts.deleteContact();
                     break;
                 case 5:
                     input = 5;
                     System.out.println("Have a great day! Goodbye.");
+                    System.exit(0);
                     break;
             }
         }
     }
+}
 
